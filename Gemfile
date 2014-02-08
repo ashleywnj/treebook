bfe8ed5c5b38f25d19170cd5bd8ba9a6b70bcab2
ashleywnj <ashleywnj@gmail.com>
2014-02-08 09:29:56 -0500

    Working with devise

--- Gemfile
+++ Gemfile
@@ -8,6 +8,10 @@ group :development do
   gem 'sqlite3'
 end
 
+# gems added by me
+gem 'devise'
+
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+def configure_strong_parameters
+devise_parameter_sanitizer.for(:sign_up) << [:user_name, :last_name, :first_name]
+end
